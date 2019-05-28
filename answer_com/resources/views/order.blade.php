79a6daa5b70f2ae5d2ec32341132b101af07997e
maangang <2648486432@qq.com>
2019-05-28 19:57:19 +0800

    这是顺序练习

---
+++ answer_com/resources/views/order.blade.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html >
+<html>
+	 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="baidu-site-verification" content="QXVdEdBQhU" />
+<link rel='canonical' href="http://mnks.jxedt.com/ckm1/sxlx/" />
+<link rel="shortcut icon" type="image/x-icon" href="//img.58cdn.com.cn/jxedt/logos/favicon.ico">
+	<title>【驾校一点通官网】一点通驾校模拟考试_学车理论考试试题_驾驶员考试科目一</title>
+<meta name="keywords" content="驾校科目一,驾考科目一,驾校科目一模拟考试">
+<meta name="description" content="驾校一点通科目一考试题库，包含2019年新的驾校科目一考试题目及答案，今年所有的科目一考试题都在里面了！快快戳进来吧！">
+<link rel="shortcut icon" type="image/x-icon" href="//img.58cdn.com.cn/jxedt/logos/favicon.ico">
+	<meta name="mobile-agent" content="format=xhtml; url=http://m.jxedt.com/mnks/ckm1/sxlx/">
+	<meta name="mobile-agent" content="format=html5; url=http://m.jxedt.com/mnks/ckm1/sxlx/">
+	<meta name="mobile-agent" content="format=wml; url=http://m.jxedt.com/mnks/ckm1/sxlx/">
+<link href="//j2.58cdn.com.cn/jxedt/mnks/css/main.css?v=20190410" type="text/css" rel="stylesheet" /><script src="//j2.58cdn.com.cn/jxedt/jquery-1.8.3.min.js"></script>
+</head>	<script>
+var picPath = "//img.58cdn.com.cn/jxedt/kaoshi/images";
+var cssPath = "//j2.58cdn.com.cn/jxedt/kaoshi/css";
+</script>	 <body>
+
+	<div style="border: 1px solid #dddddd;margin: 0px  auto;border-left: none;border-right: none;overflow: hidden;">
+	<a href="//www.jxedt.com/about/" target="_blank" onclick="clickLog('from=JXEDT_APP_dingbu')">
+		<img src="//j1.58cdn.com.cn/jxedt/m/https/images/top.png?v=20190321" style="width:100%">
+	</a>
+</div>
+<div class="top-bar main-full">
+    <div class="main">
+        <div class="pull-left">
+
+        </div>
+        <div class="pull-right">
+            <span><a href="//user.jxedt.com/home/login">登录</a></span>
+            <span><a href="//user.jxedt.com/register">注册</a></span>
+            <span><a href="//www.jxedt.com/about/help.asp">帮助</a></span>
+        </div>
+    </div>
+</div>
+<div class="main top-header">
+    <div class="pull-left">
+        <a href="//www.jxedt.com"><img src="//img.58cdn.com.cn/jxedt/logo/logo.png" width="228" height="52" alt="驾校一点通"/></a>
+    </div>
+    <div class="pull-left">
+     <a href="//www.jxedt.com/about/"  target="_blank" >
+        <img src="//j2.58cdn.com.cn/jxedt/mnks/images/logo_ad.png" alt=""></a>
+    </div>
+    <div class="pull-right">
+             <div class="top-search">
+                <select name="sort" id="sort">
+                  <option value="jx">驾校</option>
+                  <option value="jl">教练</option>
+                  <option value="pl">陪练</option>
+                  <option value="google">综合</option>
+                </select>
+
+                <div class="search-split"></div>
+                <input class="search-key" id="keyword"/><input class="search-btn" type="submit" onclick="submitform()" value="搜索"/>
+            </div>
+
+<script>
+	function submitform(){
+		var keyword = $("#keyword").val();
+		var sort = $("#sort").val();
+
+		if(keyword==""){
+			alert("请输入关键词");
+			return ;
+		}
+
+    	if (sort=="jx"){
+    		location.href = "http://jiaxiao.jxedt.com/?keywords="+keyword;
+    	}
+
+		if (sort=="jl"){
+    		location.href = "http://jl.jxedt.com/?keywords="+keyword;
+    	}
+
+		if (sort=="pl"){
+    		location.href = "http://pl.jxedt.com/?keywords="+keyword;
+    	}
+
+		if(sort == "google"){
+			location.href = "http://zhannei.baidu.com/cse/search?s=16040176402735648187&q="+keyword;
+		}
+	}
+
+    $(function(){
+        document.onkeydown = function(e){
+            var ev = document.all ? window.event : e;
+            if(ev.keyCode==13) {
+           	 	submitform();
+        	}
+        }
+    });
+</script>
+
+     </div>
+</div>
+<div class="top-menu main-full">
+    <div class="main">
+        <a href="//www.jxedt.com">首页</a>
+        <a href="//mnks.jxedt.com/" class="on" >模拟考试</a>
+        <a href="//vip.jxedt.com/" onclick="clickLog('from=JXEDT_Sy_tabVIP')">VIP试题</a>
+        <a href="//jiaxiao.jxedt.com/" onclick="clickLog('from=JXEDT_Sy_tabjiaxiao')">找驾校</a>
+        <a href="//jl.jxedt.com/" onclick="clickLog('from=JXEDT_Sy_tabjiaolian')">找教练</a>
+        <a href="//pl.jxedt.com/">找陪练</a>
+        <a style="display: none" href="//tieba.jxedt.com/">试题讨论</a>
+        <a href="//www.jxedt.com/wen/" onclick="clickLog('from=JXEDT_Sy_tabwenda')">学车问答</a>
+        <a href="//tv.jxedt.com/">视频</a>
+    </div>
+</div>
+
+
+
+	<div class="clear"></div>
+	<div class="nav main">
+		<a href="//www.jxedt.com/">首页</a> >
+<a href="//mnks.jxedt.com/">模拟考试</a> >
+<a href="//mnks.jxedt.com/ckm1/">小车科目一</a> >
+ 				     <a href="//mnks.jxedt.com/ckm1/sxlx/">顺序练习</a>
+			</div>
+	<div style='width:1190px;height:40px ;margin:10px auto'>
+	<div id=lx_top_wenzi></div>
+</div>		<div class="lx main">
+		    <div class="main_title">顺序练习<span>车型：小车</span><span>科目：科目一</span><span></span></div>
+		    <div class="lx_box">
+
+<div class="examone" id="main_M">
+
+        <div class="lx_type"></div>
+        <div class="box">
+        	<div class="loading" id="wartingdiv">
+				<img src="//img.58cdn.com.cn/jxedt/kaoshi/images/loading.gif" />
+				加载中……
+			</div>
+			<div  id="frm_main">
+	            <h1 class="title"><span id="index"></span>/<span id="totalcount">1334</span> &nbsp;<span id="t"></span></h1>
+	            <input type="hidden" name="TrueRe" id="TrueRe">
+				<input type="hidden" name="web_note_id" id="web_note_id">
+	            <div class="lx_item">
+	                <ul id="ul_answers"></ul>
+	                <div class="right" id='qustionpic'></div>
+	            </div>
+	            <div class="clear"></div>
+	            <div class="chk_an"><span id="rightanswer" style="display: none"></span></div>
+	            <div class="clear"></div>
+	            <div class="btns">
+					<a  href="javascript:void(0)"  class="btn green" onclick="go(0)" title="按键盘 → 进入下一题">下一题</a><a  href="javascript:void(0)" class="inp sp" id="sp_lxindex" style="display:none">0/0</a>
+					<a  href="javascript:void(0)"  class="btn green"  onclick="go(-1)" title="按键盘 ← 进入上一题">上一题</a>
+					<a  href="javascript:void(0)"  class="btn grey"  id="btn_jieshi" title="按键盘 B 可查看本题解释">本题解释</a>
+					<a  href="javascript:void(0)"  class="btn grey" id="btn_teiba" style="display: none">本题讨论</a>
+					<a  href="//vip.jxedt.com/ranking/" class="btn grey" target="_blank">仿真考试</a>
+
+	            </div>
+	            <div  class="info">
+	                <span><input type="checkbox" name="AutoNextTmp" id="AutoNextTmp" value="1" title="正确答题后跳转到下一题"><label for="AutoNextTmp" title="正确答题后跳转到下一题">自动跳转至下一题</label></span>
+	                <span>答对：<font id="TrueNum">0</font> 题</span><span>答错:<font id="FalseNum">0</font> 题</span><span>正确率：<font id="TruePre">0.00%</font></span><span>共 <font id="quallcount">0</font> 题转到<input type="text" id="tbtestindex" name="tbtestindex" onkeyup="userkeyup()" onkeydown="userkeydown()" onblur="userblur()" class="uan">题</span>
+	           			            </div>
+	        </div>
+        </div>
+
+</div>
+
+
+
+
+
+
+  			</div>
+		</div>
+	<div style='width:1190px;height:90px ;margin:10px auto'><div id="lx_btm_lianjie"></div></div>
+<div style='width:1190px;margin:10px auto'><div id="lx_btm_banner"></div></div>
+
+<div class="clear"></div>
+<div class="footer main-full">
+    <div class="main">
+        <div class="pull-left footer-links">
+            <p>
+            <a href="//www.jxedt.com/about/jianjie.asp" class="first" rel="nofollow"  target="_blank">网站介绍</a>|
+    <a href="//www.jxedt.com/about/sitemap.asp" target="_blank" rel="nofollow">网站地图</a>|
+    <a href="//www.jxedt.com/about/link.asp" target="_blank" rel="nofollow">合作链接</a>|
+    <a href="//www.jxedt.com/about/laws.asp" target="_blank" rel="nofollow">免责声明</a>|
+    <a href="//www.jxedt.com/about/ad.asp" target="_blank" rel="nofollow">广告服务</a>|
+    <a href="//www.jxedt.com/help.asp" target="_blank" rel="nofollow">用户帮助</a>|
+    <a href="//www.jxedt.com/about/lianxi.asp" target="_blank" rel="nofollow">联系我们</a>|
+    <a href="//www.jxedt.com/about/index.asp" target="_blank" rel="nofollow">意见反馈</a>
+
+
+
+
+            </p>
+
+            <p>
+                驾校一点通（<a href="//www.jxedt.com"  target="_blank">www.jxedt.com</a>）旗下平台： <a href="//jiaxiao.jxedt.com/" target="_blank">驾校平台</a>
+    <a href="//jl.jxedt.com/"  target="_blank">教练平台</a>
+    <a href="//pl.jxedt.com/" target="_blank">陪练平台</a>
+    <a href="//kaoshi.jxedt.com/" target="_blank">考试平台</a>
+            </p>
+
+            <p>
+                杭州联桥网络科技有限公司 <a href="//j1.58cdn.com.cn/dist/jxedt/permit/license.html" target="_blank">浙ICP备09002072号</a>
+                <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11010502037619" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
+                    <img src="https://img.58cdn.com.cn/dist/jxedt/pc/products/user/images/beian.png" style="float:left;"/> 京公网安备 11010502037619号
+                </a>
+            </p>
+        </div>
+        <div class="pull-right footer-ewm">
+        	<div style="text-align: center; display:inline-block; margin-right:30px; color:#fff">
+            <img style="margin:0" src="//j1.58cdn.com.cn/dist/jxedt/pc/products/jxregister/images/app_code.png" width=86><br>官方新题库
+            </div>
+        </div>
+    </div>
+</div>
+
+	</body>
+	<script language="javascript">
+		var allcount = 1334;
+		var sections = [[1, 365], [2541, 2640], [7674, 7676], [7681, 7682], [7689, 7713], [7715, 7722], [7729, 7729], [7738, 7746], [7749, 7760], [7766, 7766], [7773, 7785], [7797, 7797], [7800, 7805], [7808, 7829], [13474, 13477], [13479, 13504], [13594, 13605], [13607, 13613], [16509, 16511], [390, 427], [596, 602], [366, 389], [428, 595], [603, 677], [13478, 13478], [13505, 13526], [13897, 13897], [678, 864], [7677, 7680], [7683, 7688], [7714, 7714], [7723, 7728], [7730, 7737], [7747, 7748], [7761, 7765], [7767, 7772], [7786, 7796], [7798, 7799], [7806, 7807], [13606, 13606], [865, 920], [922, 973], [13527, 13554], [921, 921]];
+		var sectionList = [[1, 365, 0], [2541, 2640, 0], [7674, 7676, 0], [7681, 7682, 0], [7689, 7713, 0], [7715, 7722, 0], [7729, 7729, 0], [7738, 7746, 0], [7749, 7760, 0], [7766, 7766, 0], [7773, 7785, 0], [7797, 7797, 0], [7800, 7805, 0], [7808, 7829, 0], [13474, 13477, 0], [13479, 13504, 0], [13594, 13605, 0], [13607, 13613, 0], [16509, 16511, 0], [390, 427, 0], [596, 602, 0], [366, 389, 0], [428, 595, 0], [603, 677, 0], [13478, 13478, 0], [13505, 13526, 0], [13897, 13897, 0], [678, 864, 0], [7677, 7680, 0], [7683, 7688, 0], [7714, 7714, 0], [7723, 7728, 0], [7730, 7737, 0], [7747, 7748, 0], [7761, 7765, 0], [7767, 7772, 0], [7786, 7796, 0], [7798, 7799, 0], [7806, 7807, 0], [13606, 13606, 0], [865, 920, 0], [922, 973, 0], [13527, 13554, 0], [921, 921, 0]];
+	</script>
+	<script>
+		var carType = "c";
+		var chapterid = 0 ;
+		var kemuType = "1";
+		var cookiename = "lx_sxlx_" + carType+"km"+kemuType+"_index";
+	</script>
+		<script src="//j2.58cdn.com.cn/jxedt/kaoshi/js/common.js" type="text/javascript"></script>
+<script src="//j2.58cdn.com.cn/jxedt/mnks/js/dialog.js" type="text/javascript"></script>
+<script src="//j2.58cdn.com.cn/jxedt/mnks/js/lx.js?v=20190410" type="text/javascript"></script>     <script>
+	windowonload();
+	function windowonload() {
+	    $$("quallcount").innerHTML = allcount;
+	    if (jsgetCookie("autotonext") != "") {
+	        $$("AutoNextTmp").checked = true;
+	    }
+	    var index = jsgetCookie(cookiename);
+	    if (index == "" || index == "1") {
+	    	$$("frm_main").style.display = "block";
+	        getModel(1);
+	    }
+	    else {
+	    	 mjx.dialog.dialog("开始练习", "上次练习到第" + index + "题，是否继续？", {
+                 yes: "确定",
+                 no: "取消",
+                 yescallback: function() {
+                	 getModel(index);
+                 },
+                 nocallback: function() {
+                	 getModel(1);
+                 }
+             })
+	    }
+	    $$("wartingdiv").style.display = "none";
+	    $$("frm_main").style.display = "block";
+	}
+	</script>
+
+	<script type="text/javascript">
+	document.write('<script type="text/javascript" src="//track.58.com/adsys/jxedt_pcpagead?r='+ Math.random()+'"></scr'+'ipt>');
+</script>
+	<script type="text/javascript">
+	var _hmt = _hmt || [];
+	(function() {
+	  var hm = document.createElement("script");
+	  hm.src = "//hm.baidu.com/hm.js?b54f0f8b3b75a8b7486c9adedf28f361";
+	  var s = document.getElementsByTagName("script")[0];
+	  s.parentNode.insertBefore(hm, s);
+	})();
+</script>
+<script>
+			var _trackURL = "{'cate':'','area':'','channel':'other','pagetype':'sxlx'}";
+	</script>
+<script src="//tracklog.58.com/referrer_jxedt_pc.js"></script>
+</html>
